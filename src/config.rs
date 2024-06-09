2f022e8ab72d5b88514b4e5b009d82ec65eeddeb
zhouzhangqi <1978892854@qq.com>
2024-06-09 23:27:39 +0800

    feature: build api skeleton

---
+++ src/config.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+
+use anyhow::{bail, Result};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AppConfig {
+    pub server: ServerConfig,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ServerConfig {
+    pub port: u16,
+}
+
+impl AppConfig {
+    pub fn load() -> Result<Self> {
+        // read from  ./app.yml, or /etc/config/app.yml, or from env CHAT_CONFIG
+        let ret = match (
+            File::open("./app.yml"),
+            File::open("/etc/config/app.yml"),
+            std::env::var("CHAT_CONFIG"),
+        ) {
+            (Ok(f), _, _) => serde_yaml::from_reader(f),
+            (_, Ok(f), _) => serde_yaml::from_reader(f),
+            (_, _, Ok(f)) => serde_yaml::from_reader(File::open(f)?),
+            _ => bail!("Config file not found"),
+        };
+        Ok(ret?)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use chat::{get_router, AppConfig};
+use tokio::net::TcpListener;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{
+    fmt::Layer, layer::SubscriberExt as _, util::SubscriberInitExt as _, Layer as _,
+};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let layer = Layer::new().pretty().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+    let config = AppConfig::load()?;
+    let addr = format!("0.0.0.0:{}", config.server.port);
+
+    let app = get_router(config);
+    let listener = TcpListener::bind(&addr).await?;
+    info!("Listening on: {}", addr);
+    axum::serve(listener, app.into_make_service()).await?;
+    Ok(())
 }
